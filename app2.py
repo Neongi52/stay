2da3c36906d583783f8ece94f4bf950cba8929fb
Neongi52 <gimyeonggim51@gmail.com>
2026-07-20 13:44:13 +0900

    Change count increment to 'infinity'
    Replaced a large number with 'infinity' for count increment.

--- app2.py
+++ app2.py
@@ -4,5 +4,5 @@ st.title("카운터 앱")
 if 'count' not in st.session_state:
     st.session_state.count = 0
 if st.button("증가"):
-    st.session_state.count += 183289594824359874358975858904358904358574985743985743985743985743985743570759483759483759483759483759483759483577598437598437594837548375948375948375948375948375948375
+    st.session_state.count += infinity
 st.markdown(f"## 현재 숫자: `{st.session_state.count}`")
