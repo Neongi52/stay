7c80789d2350ffc7ef9210a2ee9db50f603d3ae9
Neongi52 <gimyeonggim51@gmail.com>
2026-07-21 12:00:59 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,5 +3,6 @@ from openai import OpenAI
 
 ai_client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-st.title("영어공부 도우미")
-st.header("단어", "문법", "어휘", "해석")
+st.header("영어공부 도우미")
+gram, voca, talk = st.colums(3)
+
